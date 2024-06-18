cbe2d69c52772e4565289f39bb0c8700a22c48c5
ergXned <126957013+ergXned@users.noreply.github.com>
2024-06-18 15:25:30 +0200

    navbar donate color update to red

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ fn NavBar() -> Element {
                         li {class: "nav-item",
                             a { class: "btn btn-outline-secondary mb-1", href: "https://explorer.ergoplatform.com/payment-request?address=9fFzKA2WHNYyXZWc4MHPtSv6YqS8jtDsZkSnAQwVaAZrYn9ojEA", "Donate",
                                 svg {
-                                    xmlns:"http://www.w3.org/2000/svg",width:"18", height:"18", fill:"currentColor", class:"bi bi-suit-heart-fill ms-2", "viewBox":"0 0 16 16",
+                                    xmlns:"http://www.w3.org/2000/svg",width:"18", height:"18", fill:"Red", class:"bi bi-suit-heart-fill ms-2", "viewBox":"0 0 16 16",
                                     path { d:"M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1"}}
                             }
                         }
