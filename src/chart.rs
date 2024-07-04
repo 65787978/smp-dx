90f083a66410a5b26d2c722852dec904236bc68f
Nederg <ergxned@proton.me>
2024-07-04 14:46:29 +0200

    chart modifications

--- src/chart.rs
+++ src/chart.rs
@@ -16,29 +16,28 @@ pub fn Chart(miner_data: MinerStats) -> Element {
     rsx!(
         div {class:"row justify-content-center",
             div {class:"col",
-                    div {class:"card text-bg-light m-1", style:"min-width: 300px; min-height: 20rem;",
-                        div {class:"card-title m-2", b {"MINER HASHRATE"},
-                            div {
-                                LineChart {
-                                    padding_top: 30,
-                                    padding_left: 100,
-                                    padding_right: 80,
-                                    padding_bottom: 30,
-                                    viewbox_width: 2100,
-                                    viewbox_height: 600,
-                                    show_grid_ticks: true,
-                                    line_width: "0.3%",
-                                    dot_size: "1%",
-                                    label_interpolation: (|y_axis| format!("{y_axis} Mh/s")) as fn(f32) -> String,
-                                    series: vec![
-                                        y_axis,
-                                    ],
-                                    labels: x_axis,
-                                }
-                            }
+                div {class:"card text-bg-light m-1",
+                    div {class:"card-title m-2", b {"MINER HASHRATE"}}
+                    div {style:"min-width: 20rem; min-height: 20rem; max-height: 25rem;",
+                        LineChart {
+                            padding_top: 30,
+                            padding_left: 100,
+                            padding_right: 80,
+                            padding_bottom: 30,
+                            viewbox_width: 1500,
+                            viewbox_height: 450,
+                            show_grid_ticks: true,
+                            line_width: "0.3%",
+                            dot_size: "1%",
+                            label_interpolation: (|y_axis| format!("{y_axis} Mh/s")) as fn(f32) -> String,
+                            series: vec![
+                                y_axis,
+                            ],
+                            labels: x_axis,
                         }
                     }
                 }
             }
+        }
     )
 }
